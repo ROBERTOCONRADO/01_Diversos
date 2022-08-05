8e0c07a6c81ebf28df40d976fcc9f2304344dda6
RobertoConrado <robertoconradorc@gmail.com>
2022-08-04 21:25:57 -0300

    spread-operator

--- Dicas-PRO/dica2.js
+++ Dicas-PRO/dica2.js
@@ -13,3 +13,18 @@ const Roberto = { ...person, ... skillsLevel }
 //alterar valores
 const robertoNewSkills = { ...Roberto, backEnd: 7}
 console.log(Roberto)
+
+
+let squaMembers = ['roberto', 'aline', 'michael'];
+
+// 'evite fazer'
+// squaMembers.push('paulo')
+// squaMembers.push('maciel')
+// squaMembers.push('marcos')
+
+
+'Jeito PRO'
+squaMembers = [...squaMembers, 'paulo', 'marcela', 'paloma']//add final
+squaMembers = ['clara', ...squaMembers, 'lucia', 'marcela', 'pietro']//add entre
+console.log(squaMembers)
+
